cedf5ed15fcbf003bec5801c57efc56dfe81ca3a
Mohammad Elahi <mohammad.h.elahii@gmail.com>
2026-09-13 13:10:30 +0330

    #7 Implement splash screen

--- app/src/main/java/com/example/hudmapapp/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/example/hudmapapp/ui/screens/SplashScreen.kt
@@ -1,10 +1,25 @@
 package com.example.hudmapapp.ui.screens
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.navigation.NavController
+import com.example.hudmapapp.ui.navigation.AppRoute
+
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(
     navController: NavController
 ) {
+    LaunchedEffect(Unit) {
+        delay(2000L)
+
+        navController.navigate(AppRoute.Home) {
+            popUpTo(AppRoute.Home) {
+                inclusive = true
+            }
+        }
+    }
+
+   SplashScreenLayout()
 }
